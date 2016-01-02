4307429ccac6026c958b4e42fa38f26d2018e008
bernardoseven <bernardoseven@gmail.com>
2016-01-02 01:47:05 +0000

    show action in Tags controller, etc

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 get '/tags' => 'tags#index'
+get '/tags/:id' => 'tags#show', as: :tag
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
